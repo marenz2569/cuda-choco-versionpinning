fba425da3c0381a8572575f97f5dca32285253e3
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2020-09-12 01:05:45 +0200

    set execution policy to allow everything

--- travis-ci/install-cuda.ps1
+++ travis-ci/install-cuda.ps1
@@ -1,5 +1,7 @@
-cd travis-ci/cuda-$CUDA
+param([Parameter(Mandatory=$true)] $Version)
+cd travis-ci/cuda-$Version
 choco pack
+choco install --verbose -s . cuda
 if ( $? -eq $false )
 {
 	Get-Content -Tail 100 C:\ProgramData\chocolatey\logs\chocolatey.log 
