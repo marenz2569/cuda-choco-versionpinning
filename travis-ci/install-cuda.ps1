628a3ff9fe59ae33b8e257362fe5ee9215e81ae4
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2020-09-14 14:52:06 +0200

    show content of install directory

--- travis-ci/install-cuda.ps1
+++ travis-ci/install-cuda.ps1
@@ -7,3 +7,6 @@ if ( $? -eq $false )
 	Get-Content -Tail 100 C:\ProgramData\chocolatey\logs\chocolatey.log 
 	Exit 1
 }
+
+ls "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$Version"
+ls "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v$Version\include"
